aaa95a7de1020ce1e5d4708f4cfbd40abf653e59
fushinianhua <15250520078@163.com>
2025-01-12 09:50:57 +0800

    11

--- MyAmbitious/temp/declarations/cc.d.ts
+++ MyAmbitious/temp/declarations/cc.d.ts
@@ -1,5 +1,5 @@
 
-    /// <reference path="D:\软件\编辑器\Creator\3.8.2\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
+    /// <reference path="C:\ProgramData\cocos\editors\Creator\3.8.2\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
     
     /**
      * @deprecated Global variable `cc` was dropped since 3.0. Use ES6 module syntax to import Cocos Creator APIs.
--- MyAmbitious/temp/declarations/jsb.d.ts
+++ MyAmbitious/temp/declarations/jsb.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="D:\软件\编辑器\Creator\3.8.2\resources\resources\3d\engine\@types\jsb.d.ts"/>
+/// <reference path="C:\ProgramData\cocos\editors\Creator\3.8.2\resources\resources\3d\engine\@types\jsb.d.ts"/>
